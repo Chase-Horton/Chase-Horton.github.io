178cd84b6b8c96e72b4c58fee7af015d91d629e2
Chase Horton <chasehortonj@gmail.com>
2021-04-26 17:01:40 -0500

    Added ASCII art to greet message

--- terminal.js
+++ terminal.js
@@ -1,3 +1,32 @@
+greeting = `oooooo   oooooo     oooo           oooo                                                         .                                                                       oooo   o8o                        
+\`888.    \`888.     .8'            \`888                                                       .o8                                                                       \`888   \`"'                        
+ \`888.   .8888.   .8'    .ooooo.   888   .ooooo.   .ooooo.  ooo. .oo.  .oo.    .ooooo.     .o888oo  .ooooo.     ooo. .oo.  .oo.   oooo    ooo     .ooooo.  ooo. .oo.    888  oooo  ooo. .oo.    .ooooo.  
+  \`888  .8'\`888. .8'    d88' \`88b  888  d88' \`"Y8 d88' \`88b \`888P"Y88bP"Y88b  d88' \`88b      888   d88' \`88b    \`888P"Y88bP"Y88b   \`88.  .8'     d88' \`88b \`888P"Y88b   888  \`888  \`888P"Y88b  d88' \`88b 
+   \`888.8'  \`888.8'     888ooo888  888  888       888   888  888   888   888  888ooo888      888   888   888     888   888   888    \`88..8'      888   888  888   888   888   888   888   888  888ooo888 
+    \`888'    \`888'      888    .o  888  888   .o8 888   888  888   888   888  888    .o      888 . 888   888     888   888   888     \`888'       888   888  888   888   888   888   888   888  888    .o 
+     \`8'      \`8'       \`Y8bod8P' o888o \`Y8bod8P' \`Y8bod8P' o888o o888o o888o \`Y8bod8P'      "888" \`Y8bod8P'    o888o o888o o888o     .8'        \`Y8bod8P' o888o o888o o888o o888o o888o o888o \`Y8bod8P' 
+                                                                                                                                  .o..P'                                                                 
+                                                                                                                                  \`Y8P'                                                                  
+                                                                                                                                                                                                         
+   .                                         o8o                        oooo                                                                                                                             
+ .o8                                         \`"'                        \`888                                                                                                                             
+.o888oo  .ooooo.  oooo d8b ooo. .oo.  .oo.   oooo  ooo. .oo.    .oooo.    888                                                                                                                             
+ 888   d88' \`88b \`888""8P \`888P"Y88bP"Y88b  \`888  \`888P"Y88b  \`P  )88b   888                                                                                                                             
+ 888   888ooo888  888      888   888   888   888   888   888   .oP"888   888                                                                                                                             
+ 888 . 888    .o  888      888   888   888   888   888   888  d8(  888   888                                                                                                                             
+ "888" \`Y8bod8P' d888b    o888o o888o o888o o888o o888o o888o \`Y888""8o o888o                                                                                                                            
+                                                                                                                                                                                                         
+                                                                                                                                                                                                         
+                                                                                                                                                                                                         \n\tType help for more information.`
+greeting1 =` _    _ ____ __   ___ _____ __  __ ____    ____ _____    __  __ _  _    _____ _  _ __   ____ _  _ ____ 
+( \\/\\/ ( ___(  ) / __(  _  (  \\/  ( ___)  (_  _(  _  )  (  \\/  ( \\/ )  (  _  ( \\( (  ) (_  _( \\( ( ___)
+ )    ( )__) )(_( (__ )(_)( )    ( )__)     )(  )(_)(    )    ( \\  /    )(_)( )  ( )(__ _)(_ )  ( )__) 
+(__/\\__(____(____\\___(_____(_/\\/\\_(____)   (__)(_____)  (_/\\/\\_)(__)   (_____(_)\\_(____(____(_)\\_(____)
+ ____ ____ ____ __  __ ____ _  _   __   __                                                             
+(_  _( ___(  _ (  \\/  (_  _( \\( ) /__\\ (  )                                                            
+  )(  )__) )   /)    ( _)(_ )  ( /(__)\\ )(__                                                           
+ (__)(____(_)\\_(_/\\/\\_(____(_)\\_(__)(__(____)                                                          `
+console.log(greeting)
 let loc = 'home'
 let fileLocs = {
     '/': ['home'],
@@ -14,7 +43,7 @@ let files = {
 
 $('#terminalDiv').terminal({
     hello: function(what) {
-        this.echo('Hello, ' + what +'. Wellcome to this terminal.');
+        this.echo('Hello, ' + what +'. Welcome to this terminal.');
         },
     help : function(){
         this.echo(`For more information on any command press type help COMMAND-NAME
@@ -87,7 +116,7 @@ mkdir DIR               |   Create a directory with name DIR`
     }
     }, 
     {
-        greetings: 'Welcome to my interactive terminal. Type help for more information.',
+        greetings: greeting,
         completion: true,
     }
 
