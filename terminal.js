0c84f7a308612c9116f35b96a9939b858784400f
Chase Horton <chasehortonj@gmail.com>
2021-04-25 21:45:06 -0500

    Added folder system

--- terminal.js
+++ terminal.js
@@ -1,7 +1,15 @@
-files = {
+let loc = 'home'
+let fileLocs = {
+    '/': ['home'],
+    'home':[]
+}
+let files = {
+    '/':{'home':'folder'},
+    'home':{
     'info.txt': 'Made with JQuery Terminal',
     'about.txt': 'Made by Chase Horton 4/25/21',
     'fizzBuzz.py': 'while True:\n   print("fizzBuzz")'
+    }
 }
 
 $('#terminalDiv').terminal({
@@ -14,23 +22,68 @@ CTRL + L                |   Clears the screen
 ls                      |   Lists files in current directory
 cat FILENAME            |   Prints contents of FILENAME
 touch FILENAME CONTENT  |   Create File with FILENAME containing CONTENT
-rm FILENAME             |   Deletes FILENAME`
+rm FILENAME             |   Deletes FILENAME
+cd LOCATION             |   Change current directory to LOCATION
+mkdir DIR               |   Create a directory with name DIR`
 );
     },
     ls : function(){
-        let keys = Object.keys(files)
-        for(let i = 0; i < keys.length; i++){
+        if(loc == undefined){
+            let keys = Object.keys(files)
+            for(let i = 0; i < keys.length; i++){
+                this.echo(keys[i]);
+            }
+        } else {
+            let keys = Object.keys(files[loc])
+            for(let i = 0; i < keys.length; i++){
             this.echo(keys[i]);
+            }
         }
+        
     },
     cat : function(fileName){
-        this.echo(files[fileName]);
+        if(files[loc][fileName] == undefined){
+            this.echo('Invalid File Name.')
+        } else{
+            this.echo(files[fileName]);
+        }
+
     },
     touch : function(fileName, fileContent){
-        files[fileName] = fileContent
+        if(files[loc][fileName] == undefined){
+            files[loc][fileName] = fileContent;
+        }
+        else{
+            this.echo('File already exists');
+        }
     },
     rm : function(fileName){
-        delete files[fileName]
+        delete files[loc][fileName];
+    },
+    mkdir : function(dirName){
+        if(!fileLocs[loc].includes(dirName)){
+            files[dirName] = {};
+            files[loc][dirName] = 'Folder'
+            fileLocs[loc].push(dirName);
+        } else{
+            this.echo('Cannot create a directory that already exists.')
+        }
+    },
+    cd : function(dirName){
+        if(dirName == '..'){
+            temp = Object.keys(fileLocs).find(key => fileLocs[key].includes(loc));
+            if(temp != undefined){
+                loc = temp;
+            } else{
+                this.echo('You are already at the root directory!')
+            }
+        }
+        else if(fileLocs[loc].includes(dirName)){
+            loc = dirName;
+        }
+        else{
+            this.echo('Directory does not exist');
+        }
     }
     }, 
     {
