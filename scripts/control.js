617ed31cb7edea3c7272c33e8b2c94c0d2d02322
Chase Horton <chasehortonj@gmail.com>
2021-05-02 14:24:24 -0500

    Added card to homepage

--- scripts/control.js
+++ scripts/control.js
@@ -1,23 +1,24 @@
 //function to getCookie from https://www.w3schools.com/js/js_cookies.asp
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 $(window).on('load', () =>{
-    $('a[href*="' + location.pathname.substring(location.pathname.lastIndexOf('/') + 1) + '"]').addClass('current');
-    if(getCookie('animation') == 'false'){
-        $('div.navBar div').removeAttr('id');
-        $('header').attr('id', 'navBar');
-    }
+  $('a[href*="' + location.pathname.substring(location.pathname.lastIndexOf('/') + 1) + '"]').addClass('current');
+  if(getCookie('animation') == 'false'){
+      $('div.navBar div').removeAttr('id');
+      $('header').attr('id', 'navBar');
+      document.getElementById("homeCard").style.animationDelay = "0.5s"
+  }
 });
